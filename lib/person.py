ca1ba8d33c1dfa087cfb4a82c8bbd82c55d395f6
Mark Njenga <marknjenga25@gmail.com>
2024-09-06 15:06:46 +0300

    "Updated Pipfile.lock and added methods to Dog and Person classes"

--- lib/person.py
+++ lib/person.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
 
 class Person:
-    # Class body goes here
 
-    #Instance method definition
-    pass
+    def talk(self):
+        print("Hello World!")
+
+    def walk(self):
+        print("The person is walking.")
+
+Person1 = Person()
+Person1.talk()
+Person1.walk()
